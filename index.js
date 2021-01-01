5573bf368337dc7fb4568f5a5837f76eac7976f5
yuki5664 <monkey.yebisu.7777@gmail.com>
2021-01-01 12:13:35 +0900

    Add node-fetch & async, await

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const https = require('https');
+// const https = require('https');
+const fetch = require('node-fetch');
 const CryptoJS = require('crypto-js');
+
+// アクセストークンの取得
+const headers = {
+    client_id: "mnytrtcna0j4uh0urkur",
+    sign: "",
+    access_token: "",
+    t: 0,
+    sign_method: "HMAC-SHA256"
+};
 
 // TODO: 2時間に一回、アクセストークンを取得する
 // 1.アクセストークンの取得
@@ -19,50 +29,14 @@ function access_calcSign(clientId, secret, timestamp){
     return signUp;
 }
 
-// アクセストークンの取得
-const headers = {
-    client_id: "mnytrtcna0j4uh0urkur",
-    sign: "",
-    access_token: "",
-    t: 0,
-    sign_method: "HMAC-SHA256"
-};
+    const timestamp = getTime();
+    const secret = "117ffe6d7b25413d8dad20f262c1a197";
+    const clientId = headers.client_id;
+    const access_token = headers.access_token;
+    const sign = calcSign(clientId, access_token, secret, timestamp);
+    headers.sign = sign;
+    headers.t = timestamp;
 
-const timestamp = getTime();
-const secret = "117ffe6d7b25413d8dad20f262c1a197";
-const clientId = headers.client_id;
-const access_token = headers.access_token;
-const sign = access_calcSign(clientId,secret,timestamp);
-headers.sign = sign;
-headers.t = timestamp;
-
-
-const options = {
-    protocol: 'https:',
-    host: 'openapi.tuyaus.com',
-    path: '/v1.0/token?grant_type=1',
-    method: 'GET',
-    headers: headers
-};
-
-// 叩いて取得したアクセストークンを変数にいれる
-const req = https.request(options, (res) => {
-    let data = '';
-    res.on('data', (chunk) => {
-        data += chunk;
-        const obj = JSON.parse(data);
-        const access_token = obj.result.access_token;
-    });
-    res.on('end', () => {
-        console.log('No more data in response.');
-    });
-})
-
-req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-});
-
-req.end();
 
 // 電流データの取得用
 function calcSign(clientId, access_token, secret, timestamp){
@@ -73,11 +47,39 @@ function calcSign(clientId, access_token, secret, timestamp){
     return signUp;
 }
 
-// 電流データの取得
-const data_headers = {
-    client_id: "mnytrtcna0j4uh0urkur",
-    access_token: "",//最新のアクセストークンで行うこと
-    sign: "",
-    t: 0,
-    sign_method: "HMAC-SHA256"
-};
+
+async function getAccesstokenApi() {
+    let AccessTokenUrl = 'https://openapi.tuyaus.com/v1.0/token?grant_type=1';
+    let options = {
+        method: 'GET',
+        headers: headers,
+    }
+    
+    var res = await fetch(AccessTokenUrl, options);
+    var responseBody = await res.json();
+    const access_token = await responseBody.result.access_token;
+    const sign = calcSign(clientId, access_token, secret, timestamp)
+
+    console.log(`このAPIから帰ってきたトークンは${access_token}です`)
+}
+
+getAccesstokenApi();
+
+
+async function getDateApi() {
+    let AccessTokenUrl = 'https://openapi.tuyaus.com/v1.0/devices/eb81d3d6ba9e2fbc75hdyr';
+    let options = {
+        method: 'GET',
+        headers: headers,
+    }
+    
+    var res = await fetch(AccessTokenUrl, options);
+    var responseBody = await res.json();
+    const access_token = await responseBody.result.access_token;
+    const sign = calcSign(clientId, access_token, secret, timestamp)
+
+    console.log(`このAPIから帰ってきたトークンは${access_token}です`)
+}
+
+
+
